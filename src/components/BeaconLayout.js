e806eaaee107402dc4e0b7c7008222e7e0980e29
Hong <tjdqls1668@ajou.ac.kr>
2021-07-09 17:15:56 +0900

    Add Beacon component

--- src/components/BeaconLayout.js
+++ src/components/BeaconLayout.js
@@ -2,7 +2,17 @@ import React from 'react';
 import Beacon from './Beacon';
 
 const BeaconLayout = ({ beaconInfo }) => {
-    return <div style={{ position: 'absoloute', zIndex: '1' }}></div>;
+    console.log(beaconInfo);
+    return (
+        <div style={{ position: 'absoloute', zIndex: '1' }}>
+            {beaconInfo.map(beacon => {
+                // <div>{beacon.minor}</div>;
+                {
+                    console.log(beacon);
+                }
+            })}
+        </div>
+    );
 };
 
 export default BeaconLayout;
