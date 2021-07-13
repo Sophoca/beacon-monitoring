38a756c74b4e2765b44a47b37f0fee76002e1c02
Hong <tjdqls1668@ajou.ac.kr>
2021-07-13 14:46:37 +0900

    Add function getting Integrated BeaconInfo

--- src/components/BeaconLayout.js
+++ src/components/BeaconLayout.js
@@ -1,9 +1,46 @@
+import axios from 'axios';
+import { useAsync } from 'react-async';
 import Beacon from './Beacon';
 
-const BeaconLayout = ({ allBeaconInfo, configSlot }) => {
+async function getLists({ URL }) {
+    const response = await axios.get(URL);
+    return response.data;
+}
+
+function IntegratedBeaconInfo(allBeaconInfo, realBeaconInfo, heightRatio, beaconSize) {
+    const integratedBeaconInfo = {};
+    allBeaconInfo.forEach(beacon => {
+        const key = Object.keys(beacon);
+        integratedBeaconInfo[key] = { top: allBeaconInfo[key].top, left: allBeaconInfo[key].left };
+    });
+    return {};
+}
+
+const BeaconLayout = ({ allBeaconInfo, realBeaconURL, configSlot }) => {
     const heightRatio = 1000.0 / configSlot.parkingLotSize.height;
     const beaconSize = 15;
-    const beacons = (
+    const { data, error, isLoading, reload } = useAsync({
+        promiseFn: getLists,
+        URL: realBeaconURL,
+        watch: allBeaconInfo
+    });
+
+    function mouseOver() {
+        return (
+            <div style={{ position: 'absolute', top: '500px', left: '500px', zIndex: '20' }}>
+                Hi
+            </div>
+        );
+    }
+
+    if (isLoading) return <div> 로딩중..</div>;
+    if (error) return <div> 에러가 발생했습니다-Status {error}</div>;
+    if (!data) return <div> 반환값 없음-Status</div>;
+
+    console.log(allBeaconInfo);
+    console.log(data);
+
+    return (
         <div
             className="BeaconLayout"
             style={{
@@ -16,18 +53,33 @@ const BeaconLayout = ({ allBeaconInfo, configSlot }) => {
                 height: '100%'
             }}
         >
-            {Object.keys(allBeaconInfo).map(beaconName => (
-                <Beacon
-                    key={beaconName}
-                    top={allBeaconInfo[beaconName].top * heightRatio - beaconSize / 2}
-                    left={allBeaconInfo[beaconName].left * heightRatio - beaconSize / 2}
-                    beaconSize={beaconSize}
-                />
-            ))}
+            <button
+                style={{
+                    color: 'white',
+                    backgroundColor: 'black',
+                    width: '100px',
+                    height: '40px',
+                    position: 'fixed',
+                    zIndex: '250',
+                    left: '370px'
+                }}
+                onClick={reload}
+            >
+                reload<br></br>beacon API
+            </button>
+            {Object.keys(allBeaconInfo).map(beaconName => {
+                return (
+                    <Beacon
+                        key={beaconName}
+                        top={allBeaconInfo[beaconName].top * heightRatio - beaconSize / 2}
+                        left={allBeaconInfo[beaconName].left * heightRatio - beaconSize / 2}
+                        beaconSize={beaconSize}
+                        onClick={mouseOver}
+                    />
+                );
+            })}
         </div>
     );
-
-    return beacons;
 };
 
 export default BeaconLayout;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,27 +1,7 @@
 import React from 'react';
-import axios from 'axios';
-import { useAsync } from 'react-async';
 import BeaconLayout from './BeaconLayout';
 
-async function getLists({ URL }) {
-    const response = await axios.get(URL);
-    return response.data;
-}
-
 const Map = ({ imageInfo, imageUrl, allBeaconInfo, configSlot, realBeaconURL }) => {
-    const { data, error, isLoading, reload } = useAsync({
-        promiseFn: getLists,
-        URL: realBeaconURL,
-        watch: allBeaconInfo
-    });
-
-    if (isLoading) return <div> 로딩중..</div>;
-    if (error) return <div> 에러가 발생했습니다-Status {error}</div>;
-    if (!data) return <div> 반환값 없음-Status</div>;
-
-    console.log(allBeaconInfo);
-    console.log(data);
-
     return (
         <div
             className="Map"
@@ -34,23 +14,13 @@ const Map = ({ imageInfo, imageUrl, allBeaconInfo, configSlot, realBeaconURL }) 
             }}
         >
             <img src={imageUrl} alt={imageInfo} height="1000px" />
-            <button
-                style={{
-                    color: 'white',
-                    backgroundColor: 'black',
-                    width: '100px',
-                    height: '40px',
-                    position: 'fixed',
-                    zIndex: '250',
-                    left: '370px'
-                }}
-                onClick={reload}
-            >
-                reload<br></br>beacon API
-            </button>
-            {allBeaconInfo ? (
-                <BeaconLayout allBeaconInfo={allBeaconInfo} configSlot={configSlot} />
-            ) : null}
+            {allBeaconInfo && (
+                <BeaconLayout
+                    allBeaconInfo={allBeaconInfo}
+                    realBeaconURL={realBeaconURL}
+                    configSlot={configSlot}
+                />
+            )}
         </div>
     );
 };
