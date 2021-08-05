be4d4d51ac29723a25022afc199d68bf31b0d99f
Hong <tjdqls1668@ajou.ac.kr>
2021-08-05 14:11:58 +0900

    Update CamDict

--- src/components/temp.py
+++ src/components/temp.py
@@ -1,6 +1,12 @@
-SPAM=[["2021/07/01","spam"],
-    ["2021/07/02","spam"],
-    ["2021/06/29","spam"],
-    ["2021/07/03","spam"]]
+arr=[132, 136, 135, 139, 137, 140, 138, 134, 131, 133, 142, 150, 143, 144, 148, 145, 146, 149, 147, 141, 157, 159, 152, 160, 156, 155, 154, 151, 158, 153]
+arr2=[]
+for p in enumerate(arr, 1):
+    arr2.append(p)
 
-for data in sorted(SPAM, reverse=True):print(data[1])
+
+arr2.sort(key=lambda x: x[1])
+# for (minor, i) in arr2:
+    # print(str(i) + ': { major: 504, minor: '+str(minor)+', type: 0},')
+
+for i in range(191, 221):
+    print(str(i)+': {major: 506, minor: '+str(i-190)+', type:0},')
