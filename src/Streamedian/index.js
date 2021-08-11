78a510d839998198af1431d190b2aefe50d13ded
Hong <tjdqls1668@ajou.ac.kr>
2021-08-11 12:03:17 +0900

    Add logic that select only 3 cams

--- src/Streamedian/index.js
+++ src/Streamedian/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import StreamedianPlayer from './StreamedianPlayer';
+import styled from 'styled-components';
+
+const PlayerDiv = styled.div`
+    padding: 10px;
+    border: 1px solid black;
+`;
 
 const App = ({ id, url, remove }) => (
-    <div>
+    <PlayerDiv
+        className="player"
+        onClick={() => {
+            console.log('onClick', id);
+        }}
+    >
         <StreamedianPlayer id={id} url={url} remove={remove}>
             <source src={url} type="application/x-rtsp" />
         </StreamedianPlayer>
-    </div>
+    </PlayerDiv>
 );
 
 export default App;
--- src/components/Camera2.js
+++ src/components/Camera2.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
 import Streamedian from '../Streamedian';
 import CameraDiv from './CameraDiv';
 
@@ -45,11 +46,100 @@ const CameraName = styled.div.attrs(props => ({
     user-select: none;
 `;
 
+const initialState = {
+    cams: [
+        {
+            id: 'player0',
+            camsNum: 0,
+            major: 0,
+            minor: 0,
+            active: true
+        },
+        {
+            id: 'player1',
+            camsNum: 0,
+            major: 0,
+            minor: 0,
+            active: false
+        },
+        {
+            id: 'player2',
+            camsNum: 0,
+            major: 0,
+            minor: 0,
+            active: false
+        }
+    ]
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'SELECT':
+    }
+}
+
 const Camera2 = ({ cameraInfo, heightRatio }) => {
-    const [cams, setCams] = useState([]);
+    const [cams, setCams] = useState([
+        {
+            id: '0',
+            camNum: 0,
+            major: 0,
+            minor: 0,
+            active: true
+        },
+        {
+            id: '1',
+            camNum: 0,
+            major: 0,
+            minor: 0,
+            active: false
+        },
+        {
+            id: '2',
+            camNum: 0,
+            major: 0,
+            minor: 0,
+            active: false
+        }
+    ]);
+
+    const onCreate = useCallback(
+        camInfo => {
+            const isContain = !cams.map(cam => cam.camNum).includes(camInfo.camNum);
+            isContain ? (
+                setCams(
+                    cams.map(cam =>
+                        cam.active
+                            ? {
+                                  ...cam,
+                                  camNum: camInfo.camNum,
+                                  major: camInfo.major,
+                                  minor: camInfo.minor,
+                                  active: false
+                              }
+                            : cam
+                    )
+                )
+            ) : (
+                <Snackbar autoHideDuration={1000}>Already selected camera!</Snackbar>
+            );
+        },
+        [cams]
+    );
+
+    const onRemove = useCallback(id => {
+        setCams(cams.map(cam => (cam.id === id ? { ...cam, camNum: 0, major: 0, minor: 0 } : cam)));
+    });
+
     const cameraSize = 24;
     const create = camInfo => {
-        if (!cams.map(cam => cam.camNum).includes(camInfo.camNum)) setCams(cams.concat(camInfo));
+        let flag = true;
+        if (cams.length === 3) {
+            flag = false;
+            alert("Can't select more than 3 players");
+        }
+        if (!cams.map(cam => cam.camNum).includes(camInfo.camNum) && flag)
+            setCams(cams.concat(camInfo));
     };
     const remove = camNum => {
         setCams(cams.filter(cam => cam.camNum !== camNum));
@@ -76,7 +166,7 @@ const Camera2 = ({ cameraInfo, heightRatio }) => {
                                     size="small"
                                     className="beacon-toggle-btn"
                                     onClick={() =>
-                                        create({
+                                        onCreate({
                                             camNum: camNum,
                                             major: current.ip.major,
                                             minor: current.ip.minor
@@ -104,7 +194,7 @@ const Camera2 = ({ cameraInfo, heightRatio }) => {
                 })}
             </CameraLayoutDiv>
             <div
-                className="player"
+                className="player-container"
                 style={{
                     position: 'fixed',
                     display: 'flex',
@@ -112,21 +202,27 @@ const Camera2 = ({ cameraInfo, heightRatio }) => {
                     alignItems: 'center',
                     justifyContent: 'flex-start',
                     margin: 10 + 'px',
-                    gap: 10,
                     flexWrap: 'wrap',
                     overflow: 'auto',
                     minHeight: 'min-content'
                 }}
             >
                 {console.log('cams', cams)}
-                {cams.map(cam => (
-                    <Streamedian
-                        key={cam.camNum}
-                        id={cam.camNum}
-                        url={`rtsp://admin:admin1234@218.153.209.100:${cam.major}/cam/realmonitor?channel=${cam.minor}&subtype=1`}
-                        remove={remove}
-                    ></Streamedian>
-                ))}
+                {cams.map(cam => {
+                    const url =
+                        cam.major === 0 && cam.minor === 0
+                            ? null
+                            : `rtsp://admin:admin1234@218.153.209.100:${cam.major}/cam/realmonitor?channel=${cam.minor}&subtype=1`;
+                    // return (
+                    //     <Streamedian
+                    //         key={cam.camNum}
+                    //         id={cam.camNum}
+                    //         url={url}
+                    //         // url={`rtsp://admin:admin1234@218.153.209.100:${cam.major}/cam/realmonitor?channel=${cam.minor}&subtype=1`}
+                    //         remove={remove}
+                    //     ></Streamedian>
+                    // );
+                })}
                 {/* {ID && <Streamedian id={ID} url={url}></Streamedian>}
                 {ID && (
                     <Streamedian
