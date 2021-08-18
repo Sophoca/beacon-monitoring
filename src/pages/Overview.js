b714f57103db7572950cd6410a0503347288ea0e
Hong <tjdqls1668@ajou.ac.kr>
2021-08-18 16:10:37 +0900

    Remove Overview graph

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -1,12 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import MyResponsiveLine from '../components/MyResponsiveLine';
-
-// async function getLists({ URLs }) {
-//     axios.all(URLs.map(URL))
-//     const response = await axios.get(URL);
-//     return response.data;
-// }
 
 const Overview = ({ slotURL }) => {
     const [lists, setLists] = useState([]);
@@ -19,15 +12,17 @@ const Overview = ({ slotURL }) => {
     };
     useEffect(() => {
         getLists();
+        console.log('slotURL', slotURL);
+
         // const interval = setInterval(() => getLists(), 10000);
         // return () => clearInterval(interval);
         // eslint-disable-next-line
     }, []);
+    console.log('lists', lists);
 
     const calcTime = slotInfo =>
         slotInfo.reduce((obj, slot) => {
-            obj[slot.modified_on.split(':')[0]] = (obj[slot.modified_on.split(':')[0]] || 0) + 1;
-            // obj[slot.modified_on] = (obj[slot.modified_on] || 0) + 1;
+            obj[slot.modified_on] = (obj[slot.modified_on] || 0) + 1;
             return obj;
         }, {});
 
@@ -43,38 +38,14 @@ const Overview = ({ slotURL }) => {
                 data: calcTime(lists[idx])
             }));
             setData(response);
-            // console.log(response);
+            console.log('data', response);
         }
     }, [slotURL, lists]);
 
-    // const getChartData = timeInfo => {
-    //     const response = timeInfo.map(el => {
-    //         const data = Object.keys(el.data).map(el2 => ({ x: el2, y: el.data[el2] }));
-    //         return { id: el.location, data: data };
-    //     });
-    //     console.log(response);
-    //     return response;
-    // };
-    const getChartData = el => {
-        const data = Object.keys(el.data).map(el2 => ({ x: el2, y: el.data[el2] }));
-        return [{ id: el.location, data: data }];
-    };
-
-    // console.log('data', data);
     return (
         <div style={{ display: 'flex', overflow: 'auto', flexDirection: 'column' }}>
             overview
-            {data.map(el => {
-                return (
-                    <div style={{ height: 500 + 'px' }}>
-                        <MyResponsiveLine key={el.location} data={getChartData(el)}>
-                            {/* {el.location}: {getTime(el.data)} */}
-                        </MyResponsiveLine>
-                    </div>
-                );
-            })}
             {/* {console.log(getChartData(data))} */}
-            {/* <MyResponsiveLine data={getChartData(data)} /> */}
         </div>
     );
 };
